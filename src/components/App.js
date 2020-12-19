c16a4a1c594e5e2482b612b4316497e47cf2bc27
Aliaksandr Lahunou <mymail15-87y@yandex.ru>
2020-12-19 16:09:53 +0300

    feat: import styles, html, create Header

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,23 @@
-import './App.css';
+import Header from "./Header/Header";
+
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <div className="page">
+      <Header />
+    <main className="content">
+      <section className="hero page__hero">
+        <h1 className="title hero__title">Сайт гражданских инициатив</h1>
+        <button
+          className="btn hero__button"
+          type="button"
+        >Создать инициативу</button>
+      </section>
+    </main>
+      <footer className="footer page__footer">
+        <p className="footer__copyright">© 2020. Group 39</p>
+      </footer>
+  </div>
   );
 }
 
