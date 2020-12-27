448af942af0a5622d23e9929a71da38911f5660a
Aliaksandr Lahunou <mymail15-87y@yandex.ru>
2020-12-27 18:14:58 +0300

    feat: inputPage

--- src/components/InputPage/InputPage.js
+++ src/components/InputPage/InputPage.js
@@ -30,54 +30,57 @@ function InputPage({
   }
 
   return (
-    <section className="application page__application">
-      <div className="application__top">
-        <ButtonArrow history={history} />
-        <p className="application__counter">2/3</p>
-      </div>
-      <h2 className="title application__title">Введите обращение</h2>
-      <p className="application__subtitle">Не больше 6 слов</p>
-      <InputForm onChange={onChange} value={value} onSearchSubmit={onSearchSubmit} invalidInput={invalidInput} />
+    <section className="application">
+      <div className="application__diagonal-box"></div>
+      <div className="application__content">
+        <div className="application__top">
+          <ButtonArrow history={history} />
+          <p className="application__counter">2/3</p>
+        </div>
+        <h2 className="title application__title">Введите обращение</h2>
+        <p className="application__subtitle">Не больше 6 слов</p>
+        <InputForm onChange={onChange} value={value} onSearchSubmit={onSearchSubmit} invalidInput={invalidInput} />
 
-      <div className="form-strokes form-strokes__columns">
-        <ul className="form-strokes__list">
-          {currentArr[0] &&
-            currentArr.map((item) => {
-              return (
-                <StrChoice
-                  key={item.id}
-                  onRefresh={onRefresh}
-                  onAdd={onAddDelete}
-                  onNewSearch={onNewSearch}
-                  onEdit={onEdit}
-                  isEdit={isEdit}
-                  item={item}
-                >
-                  {MyComponent(item.strForRender, "form-strokes__input")}
-                </StrChoice>
-              );
-            })}
-        </ul>
-        <div className="assembly form-strokes__assembly">
-          <ul className="assembly__list">
+        <div className="form-strokes form-strokes__columns">
+          <ul className="form-strokes__list">
             {currentArr[0] &&
               currentArr.map((item) => {
                 return (
-                  <StrSelected key={item.id} isSelected={isSelected[item.id]} onDelete={onAddDelete} id={item.id}>
-                    {MyComponent(item.strForRender, "assembly__text")}
-                  </StrSelected>
+                  <StrChoice
+                    key={item.id}
+                    onRefresh={onRefresh}
+                    onAdd={onAddDelete}
+                    onNewSearch={onNewSearch}
+                    onEdit={onEdit}
+                    isEdit={isEdit}
+                    item={item}
+                  >
+                    {MyComponent(item.strForRender, "form-strokes__input")}
+                  </StrChoice>
                 );
               })}
           </ul>
+          <div className="assembly form-strokes__assembly">
+            <ul className="assembly__list">
+              {currentArr[0] &&
+                currentArr.map((item) => {
+                  return (
+                    <StrSelected key={item.id} isSelected={isSelected[item.id]} onDelete={onAddDelete} id={item.id}>
+                      {MyComponent(item.strForRender, "assembly__text")}
+                    </StrSelected>
+                  );
+                })}
+            </ul>
+          </div>
         </div>
+        <Button
+          type="button"
+          name="Далее"
+          element="btn_size_m application__control"
+          onClick={() => history.push("/user-submit")}
+          disabled={Object.keys(strForSubmit).length ? false : true}
+        />
       </div>
-      <Button
-        type="button"
-        name="Далее"
-        element="application__control"
-        onClick={() => history.push("/user-submit")}
-        disabled={Object.keys(strForSubmit).length ? false : true}
-      />
     </section>
   );
 }
