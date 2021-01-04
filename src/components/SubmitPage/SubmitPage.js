4dfbe90f4024f2aa9850b30e76bf65d243bec596
Aliaksandr Lahunou <mymail15-87y@yandex.ru>
2021-01-04 22:19:16 +0300

    feat:adaptive for SubmitPage

--- src/components/SubmitPage/SubmitPage.js
+++ src/components/SubmitPage/SubmitPage.js
@@ -1,3 +1,5 @@
+import MediaQuery from "react-responsive";
+
 import ButtonArrow from "../Buttons/ButtonArrow";
 import ApplicationForm from "./AplicationForm";
 
@@ -6,10 +8,12 @@ function SubmitPage({ history, strsForRender, onSubmit }) {
     <section className="application">
       <div className="application__diagonal-box"></div>
       <div className="application__content">
-        <div className="application__top">
-          <ButtonArrow history={history} />
-          <p className="application__counter">3/3</p>
-        </div>
+        <MediaQuery minWidth={761}>
+          <div className="application__top">
+            <ButtonArrow history={history} />
+            <p className="application__counter">3/3</p>
+          </div>
+        </MediaQuery>
         <ApplicationForm history={history} strsForRender={strsForRender} onSubmit={onSubmit} />
       </div>
     </section>
